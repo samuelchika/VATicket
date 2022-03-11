b802a95fa721f2f103d4eb3bd869d477bebbb24d
schika1 <schika@cloudhight.com>
2022-03-11 01:31:39 +0000

    code refactor

--- app.js
+++ app.js
@@ -6,17 +6,14 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-
 const methodOverride = require("method-override");
 const ErrorHandler = require("./Error/ErrorHanlder");
 // model for mongoose
 const TicketUser = require("./models/ticketuser");
-
 // importing routes
 const ticketsRoute = require("./routes/ticket");
 const summaryRoute = require("./routes/summary");
 const userRoute = require("./routes/user");
-
 const app = express();
 // connect to mongoose db
 main()
@@ -30,16 +27,13 @@ app.set("view engine", "ejs");
 //const path = require('path')
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(path.resolve(), "public")));
-
 const port = process.env.PORT || 5000;
-
 // for form data
 app.use(
   express.urlencoded({
     extended: true
   })
 );
-
 // session
 const sessionConfig = {
   httpOnly: true,
@@ -51,21 +45,15 @@ const sessionConfig = {
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
 };
-
 app.use(session(sessionConfig));
-
 app.use(flash());
-
 // we start working on passport from here
 app.use(passport.initialize());
 app.use(passport.session()); // this is for the passport session
-
 passport.use(new LocalStrategy(TicketUser.authenticate()));
-
 // serialize and deserialize user
 passport.serializeUser(TicketUser.serializeUser());
 passport.deserializeUser(TicketUser.deserializeUser());
-
 // making flash global.
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
@@ -75,17 +63,13 @@ app.use((req, res, next) => {
 
   next();
 });
-
 // routes
 app.use("/", userRoute);
 app.use('/tickets', ticketsRoute);
 app.use('/tickets/:id/summary', summaryRoute);
-
-
 app.get("/", (req, res) => {
   res.render("pages/index");
 });
-
 // unknown pages redirect
 app.all("*", (req, res, next) => {
   res.send(new ErrorHandler("Page Not found", 404));
